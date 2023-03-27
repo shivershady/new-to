d71eb56f488f816d36630e69dad1f7411f825ebc
Hoang Hieu <shivershady@gmail.com>
2023-03-27 09:57:17 +0700

    create container tailwind , handle click menu header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '2.5rem',
+        xl: '2.5rem',
+        '2xl': '2.5rem',
+      },
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   plugins: [],
 }
