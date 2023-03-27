98150dc26fcde949f8c8bf95e33df549b43b51f5
Hoang Hieu <shivershady@gmail.com>
2023-03-27 11:18:50 +0700

    update

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+      filename: '../index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/img', to: 'img' }
+      ]
+    })
+  ]
+};
